4089f09f279d576a582aa05d1604047f3ec001d1
i6973-er <65159840+i6973-er@users.noreply.github.com>
2020-08-22 13:59:01 +0530

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,30 @@
+class Enemy extends BaseClass {
+    constructor(x, y){
+      super(x,y,50,50);
+      
+      this.Visiblity = 255;
+    }
+  
+   display(){
+     console.log(this.body.speed);
+     if(this.body.speed < 3){
+      super.display();
+     }
+     else{
+       World.remove(world, this.body);
+       push();
+       this.Visiblity = this.Visiblity - 5;
+       tint(255,this.Visiblity);
+       
+       pop();
+     }
+     
+     
+   }
+  
+   score(){
+    if(this.Visiblity<0 && this.Visiblity > -100000){
+     score++
+    }
+  
+   }}
